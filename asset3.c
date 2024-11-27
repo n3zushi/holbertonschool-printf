9786e0cca2e69a694fc51e4c40049b8c3d392568
n3zushi <9836@holbertonstudents.com>
2024-11-27 13:38:17 +0100

    Format code in asset_print_percent function for consistency

--- asset3.c
+++ asset3.c
@@ -9,8 +9,8 @@
 
 int asset_print_percent(va_list args)
 {
-    (void)args;
-    if (write(1, "%", 1) == -1)
-        return (-1);
-    return (1);
+	(void)args;
+	if (write(1, "%", 1) == -1)
+		return (-1);
+	return (1);
 }
